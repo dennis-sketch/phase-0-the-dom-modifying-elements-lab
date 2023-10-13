690378e1ebf7b5a0a01aeb06bbe002bb4cb1e60e
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-13 07:44:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+// Write your code here
+
+
+    // Remove the 'main#main' node
+    document.getElementById('main').remove();
+
+    // Create a new 'h1' element with an id of 'victory'
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+
+    // Set the inner text of the 'h1' element
+    newHeader.innerText = 'YOUR-NAME is the champion';
+
+    // Append the 'h1' element to the document body
+    document.body.appendChild(newHeader);
+
+    // Assign the 'newHeader' variable to the created 'h1' element
+    const newHeaderVariable = document.getElementById('victory');
+
